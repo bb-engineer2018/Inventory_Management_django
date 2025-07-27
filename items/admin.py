99ca4fc4d2bbec5f41aa56d4abbcb3dec6e107a8
honda <87609013+bb-engineer2018@users.noreply.github.com>
2025-07-28 02:07:35 +0900

    Register models in admin site

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ManagementMethod, Item, Order, DeliveredOrder
 
 # Register your models here.
+admin.site.register(ManagementMethod)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(DeliveredOrder)
